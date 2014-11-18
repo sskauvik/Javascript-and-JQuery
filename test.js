85a22f2b16af021e2d43c45ab119f7af13caa4af
Sigurd Skauvik <sigurdsk@live.no>
2014-11-18 21:04:01 +0100

    Initial commit

---
+++ test.js
@@ -0,0 +1,13 @@
+window.onload = function() {
+
+ 
+
+    alert( "Velkommen til denne JavaScript test siden.(C)Sigurd 2014" );
+
+ 
+
+}
+
+$("Denne teksten er lagt inn før paragrafen").insertAfter("#PF");
+ 
+
