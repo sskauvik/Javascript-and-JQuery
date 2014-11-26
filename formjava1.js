3f77d59f2059fc71537ae167753341e36fb5984d
Sigurd Skauvik <sigurdsk@live.no>
2014-11-26 16:27:40 +0100

    Add a new javascript thing that looks kinda cool so added some code to it and change the form1.html abit..

--- formjava1.js
+++ formjava1.js
@@ -6,5 +6,14 @@ $('#myinput').on('change',function(ev){
 .on('focus',function(ev) {
   console.log('Focus is good');
 });
+});
 
+$(document).ready(function(){
+$('.parent div').on('click',function(ev) {
+  $(ev.currentTarget).animate({width:200},{duration:500});
+});
+
+$('#stretchAll').on('click', function(ev) {
+  $('.parent div').css('width','auto');
+});
 });
